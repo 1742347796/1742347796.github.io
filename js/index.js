7c58c3dfd5ff6fe7bb9f7bafa9622479d5e462d0
1742347796 <1742347796@qq.com>
2017-07-05 20:49:43 +0800

    1234

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function () {
-
+	
 	//加载fullpage
 	$('#dowebok').fullpage({
 		sectionsColor: ['#4dd0e1', '#22c3aa', '#26B1B6', '#fb6d7e', '#4dd0e1', '#e8b118'],
@@ -7,22 +7,19 @@ $(function () {
 		afterLoad: loadAnimation,
 		onLeave: leaveAnimation,
 	});
-	
-	$("#page1-model").click(function () {
+	//第一页弹框
+	$("#page1-button").click(function () {
 		$('.model').fadeIn();
 		$('.model ul li').slideDown();
 		event.stopPropagation();
 	});
-	
-//	$('.page2 ul').on('click', 'h4', function () {
-//		console.log($(event.target).html());
-//		$('.page2 .model').fadeIn();
-//		event.stopPropagation();
-//	});
-	
+	//第二页弹框
+	$('.page2 ul li').click(function () {
+		showModel($(this));
+	});
+	//清除弹框
 	$(document).click(function(){  
-       $('.model').fadeOut();
-       $('.model ul li').slideUp();
+		hideModel();
     });  
     
 	$('.page1 .container img').delay(800).animate({'opacity': '1'}, 500);
@@ -35,7 +32,9 @@ $(function () {
 	
 	function leaveAnimation (anchorLink, index) {
 		var time = 1200;
-			
+		
+		hideModel('fast');
+		
 		if (index === 2) {
 			
 			$('.skill h4').eq(0).animate({'width': '100%'}, time);
@@ -59,22 +58,18 @@ $(function () {
 		if (index === 5) {
 			var s = $('.page5 .an-hide');
 			for (var i = 0; i < s.length; i ++) {
-				var random = parseInt(3000 * Math.random());
+				var random = parseInt(Math.random() * (3000 - 500) + 500);
 				$(s[i]).animate({'opacity': '1'}, random);
-				$('.page5 .an-down').animate({'margin-top': '0%'}, random);
 			}
-			
 		}
 		
 		if (index === 6) {
-			
 			$('.page6 .an-down').animate({'margin-top': '0%'}, time);
 			$('.page6 .an-hide').delay(time).animate({'opacity': '1'}, 100);
 			shake($('.page6 .an-hide'));
 		}
 		return ;
 	}
-	
 	
 });
 
@@ -95,4 +90,39 @@ function shake (dom) {
 	$(dom).animate({'margin': '5px 0 0 15px'}, timeShake);
 	$(dom).animate({'margin': '5px 5px 0 0'}, timeShake);
 	$(dom).animate({'margin': '5px 0 0 0'}, timeShake);	
+}
+
+//展示第二页弹窗
+function showModel (target) {
+	
+	var skill = [['能使用合理的结构和样式编写兼容主流浏览器的页面;', '能适当运用CSS3提升页面的体验效果', '能熟练运用Chrome开发者工具进行页面调试'],
+		['熟悉原生js，可完成常见的数据处理、页面交互等工作', '熟悉闭包、原型链，熟悉ES6新特性', '能运用模块化、面向对象的思想编程'],
+		['能编写低耦合、复用性高的vue组件', '熟悉组件数据处理、通信。能利用vuex进行状态管理'],
+		['能运用jQuery完成高效、清晰的前端开发', '能根据需要编写实用的插件'],
+		['能完成兼容PC端、移动端的响应式设计', '能合理运用Bootstrap样式和布局提升页面效果'],
+		['了解JavaWeb后端开发，MVC设计模式，SHH框架','编写过基于Hibernate，Struts等技术的后端模块','熟悉面向对象编程思想，开发过飞行棋、CSV文件处理等Java小程序']],	
+		
+		index = target.index();
+	
+	if ($('.page2 .model').css('display') !== 'block') {
+		$('.page2 .model ul li').html('&nbsp;');
+		for (var i = 0; i < skill[index].length; i ++) {
+			$('.page2 .model ul li').eq(i).html(skill[index][i]); 
+		}
+		$('.page2 .model').fadeIn();
+		$('.model ul li').slideDown();
+		event.stopPropagation();
+	}
+	
+}
+
+//隐藏第二页弹窗
+function hideModel (type) {
+	if (type || type === 'fast') {
+		$('.model').hide();
+	} else {
+		$('.model').fadeOut();
+	}
+	
+    $('.model ul li').slideUp();
 }
